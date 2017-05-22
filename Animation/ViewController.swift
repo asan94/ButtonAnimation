04810028e7aa393193f6ea0dec24417316514859
biceps_one_love <ametov.asan.d@gmail.com>
2017-05-22 16:36:25 +0300

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Аметов Асан on 22.05.17.
+//  Copyright © 2017 Asan Ametov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
